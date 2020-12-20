beb093d3c1d421015c7766c59433161d74cb3ed3
dinagouda <dinagouda1@gmail.com>
2020-12-20 17:16:09 +0200

    first commit

---
+++ Js/custom.js
@@ -0,0 +1,17 @@
+
+$(function () {
+
+	"use strict";
+
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+
+});
+
+
+// ---------------------Slider js-------------------
+
